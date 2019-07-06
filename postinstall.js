2316ab8722a8490b5142a22748435a8091b81491
mutoe <mutoe@foxmail.com>
2019-07-06 09:59:19 +0800

    style: 自定义代码风格

--- postinstall.js
+++ postinstall.js
@@ -6,29 +6,29 @@
  */
 
 /* eslint-disable no-undef,@typescript-eslint/no-var-requires,no-console */
-const rm = require("rimraf");
-const pkg = require("./package.json");
+const rm = require('rimraf')
+const pkg = require('./package.json')
 
 /**
  * @type string[]
  */
-const dev = Object.keys(pkg.devDependencies) || [];
+const dev = Object.keys(pkg.devDependencies) || []
 /**
  * 删除多余的`@types`文件
  */
 rm(
-    `node_modules/@types/**/*`,
-    {
-        glob: {
-            ignore: dev.map((s) => `node_modules/${s}/**`),
-        },
+  `node_modules/@types/**/*`,
+  {
+    glob: {
+      ignore: dev.map((s) => `node_modules/${s}/**`),
     },
-    (err) => {
-        if (err) {
-            console.err(err);
-            console.err("× @types 删除失败!");
-        } else {
-            console.log("√ @types 清理完成.");
-        }
-    },
-);
+  },
+  (err) => {
+    if (err) {
+      console.err(err)
+      console.err('× @types 删除失败!')
+    } else {
+      console.log('√ @types 清理完成.')
+    }
+  },
+)
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { GlobalData } from "/model/global-data";
+import { GlobalData } from '/model/global-data'
 
 //app.ts
 
@@ -6,47 +6,47 @@ import { GlobalData } from "/model/global-data";
  * 认证
  */
 function auth(): Promise<wx.GetUserInfoSuccessCallbackResult> {
-    return new Promise<wx.GetUserInfoSuccessCallbackResult>((resolve, reject) => {
-        wx.getUserInfo({
-            success: resolve,
-            fail: reject,
-        });
-    });
+  return new Promise<wx.GetUserInfoSuccessCallbackResult>((resolve, reject) => {
+    wx.getUserInfo({
+      success: resolve,
+      fail: reject,
+    })
+  })
 }
 
 /**
  * 获取用户信息
  */
-async function getUserInfo(): Promise<wx.GetUserInfoSuccessCallbackResult["userInfo"]> {
-    const userInfo = await auth();
-    return userInfo.userInfo;
+async function getUserInfo(): Promise<wx.GetUserInfoSuccessCallbackResult['userInfo']> {
+  const userInfo = await auth()
+  return userInfo.userInfo
 }
 
 App({
-    onLaunch() {
-        console.log("{{VERSION}}");
-        // 展示本地存储能力
-        const logs: number[] = wx.getStorageSync("logs") || [];
-        logs.unshift(Date.now());
-        wx.setStorageSync("logs", logs);
-        // 登录
-        wx.login({
-            success(_res) {
-                // JSON.stringify(this);
-                console.log(_res.code);
-                // setTimeout();
-                // 发送 _res.code 到后台换取 openId, sessionKey, unionId
-            },
-        });
-        // 获取用户信息
-        wx.getSetting({
-            success: (res) => {
-                //@ts-ignore
-                if (res.authSetting["scope.userInfo"]) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                }
-            },
-        });
-        getUserInfo().then((res) => (GlobalData.userInfo = res));
-    },
-});
+  onLaunch() {
+    console.log('{{VERSION}}')
+    // 展示本地存储能力
+    const logs: number[] = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    // 登录
+    wx.login({
+      success(_res) {
+        // JSON.stringify(this);
+        console.log(_res.code)
+        // setTimeout();
+        // 发送 _res.code 到后台换取 openId, sessionKey, unionId
+      },
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: (res) => {
+        //@ts-ignore
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        }
+      },
+    })
+    getUserInfo().then((res) => (GlobalData.userInfo = res))
+  },
+})
